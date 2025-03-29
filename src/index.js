88f23a7d07a7727a9ff65125bba033506f02250b
Marcello Crescimbeni <giosuemc@gmail.com>
2025-03-28 20:35:53 -0500

    ajustes al index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ import cors from 'cors';
 import { routes } from './mail.controller.js';
 import { errorHandler } from './errorHandler.js';
 
-const PORT = process.env.PORT ? process.env.PORT : 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
-app.use(errorHandler);
-app.use(routes);
 app.use(
   cors({
     origin: [
-      'https://multiple-services-765e6.web.app/',
-      'https://multiple-services-765e6.firebaseapp.com/',
-      'http://localhost:3000/',
+      'https://multiple-services-765e6.web.app',
+      'https://multiple-services-765e6.firebaseapp.com',
+      'http://localhost:3000',
     ],
+    methods: ['POST', 'OPTIONS'],
+    credentials: true,
   })
 );
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.use(errorHandler);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
 });
